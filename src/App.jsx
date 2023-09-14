c3e840f61db6a3fb5ef25e00ccc5c7d6a8380273
orreuven <orreuven1243@gmail.com>
2023-09-14 12:19:33 +0300

    finished template

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { UserContext } from "./context/userContext";
+import { useContext } from "react";
+
+const App = () => {
+  const { users } = useContext(UserContext);
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route></Route>
+      </Routes>
+      {users.map((user) => (
+        <p key={user.username}>{user.username}</p>
+      ))}
+    </>
+  );
+};
+
+export default App;
