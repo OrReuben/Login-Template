ee82a2d91176575eb52848043c1dd4c0608b684b
orreuven <orreuven1243@gmail.com>
2023-09-18 13:57:14 +0300

    added data

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import data from './data.json'
 
 const App = () => {
   const { users } = useContext(UserContext);
-  const change = "navbar-1"
+  const change = "master-1"
   console.log(change);
   console.log(data);
 
