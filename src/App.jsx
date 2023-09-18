65adb07480a1dee10ea5ebf9d277b60a307b58cd
orreuven <orreuven1243@gmail.com>
2023-09-18 11:25:22 +0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { useContext } from "react";
 
 const App = () => {
   const { users } = useContext(UserContext);
-  const change = "master-1"
+  const change = "navbar-1"
   console.log(change);
 
   return (
